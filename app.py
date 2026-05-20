ff770c6c9949df330e758bc5ed63bda2dc50bb82
Thanoo <homefoodautomation@gmail.com>
2026-05-20 12:11:13 +0700

    update feature

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ import requests
 import time
 import firebase_admin
 from firebase_admin import credentials, firestore
-from flask_cors import CORS
+#from flask_cors import CORS
 
 # =========================================================
 # FLASK
 # =========================================================
 app = Flask(__name__)
-CORS(app)
+#CORS(app)
 
 # =========================================================
 # ENV
